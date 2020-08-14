e9deabd829b839ff8878f586a2cee7c3a13462a4
Purushotham <srimca87@yahoo.com>
2020-08-14 16:54:02 +0530

    changes

--- www/MathCalculator.js
+++ www/MathCalculator.js
@@ -1,11 +1,13 @@
 var exec = require('cordova/exec');
 
-module.coolMethod = function (arg0, success, error) {
+module.exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'MathCalculator', 'coolMethod', [arg0]);
 };
-module.add = function(arg0, success, error){
+
+module.exports.add = function(arg0, success, error){
     exec(success, error, 'MathCalculator', 'add', [arg0])
 }
-module.substract = function(arg0, success, error){
+module.exports.substract = function(arg0, success, error){
     exec(success, error, 'MathCalculator', 'substract', [arg0])
 }
+
